65c9556b22f6a8babb0052e22fcc0a1581d7f517
Spencer Hilvitz <spencer.hilvitz@colorado.edu>
2017-07-18 22:43:56 -0600

    add image placeholder, finish accordion tab

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
-var acc = document.getElementsByClassName("accordion");
-console.log(acc);
-var i;
+// tabbed content
+   // http://www.entheosweb.com/tutorials/css/tabs.asp
+   $(".tab_content").hide();
+   $(".tab_content:first").show();
 
-for (i = 0; i < acc.length; i++) {
-    acc[i].onclick = function(){
-        this.classList.toggle("active");
+ /* if in tab mode */
+   $("ul.tabs li").click(function() {
 
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }
-    }
-}
+     $(".tab_content").hide();
+     var activeTab = $(this).attr("rel");
+     $("#"+activeTab).fadeIn();
+
+     $("ul.tabs li").removeClass("active");
+     $(this).addClass("active");
+
+   $(".tab_drawer_heading").removeClass("d_active");
+   $(".tab_drawer_heading[rel^='"+activeTab+"']").addClass("d_active");
+
+   });
+ /* if in drawer mode */
+ $(".tab_drawer_heading").click(function() {
+
+     $(".tab_content").hide();
+     var d_activeTab = $(this).attr("rel");
+     $("#"+d_activeTab).fadeIn();
+
+   $(".tab_drawer_heading").removeClass("d_active");
+     $(this).addClass("d_active");
+
+   $("ul.tabs li").removeClass("active");
+   $("ul.tabs li[rel^='"+d_activeTab+"']").addClass("active");
+   });
+
+
+ /* Extra class "tab_last"
+    to add border to right side
+    of last tab */
+ $('ul.tabs li').last().addClass("tab_last");
+ 
