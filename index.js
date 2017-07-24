d373db1dc9c18b20d593312a2a37778e075a7307
Spencer Hilvitz <spencer.hilvitz@colorado.edu>
2017-07-23 22:06:49 -0600

    work on responsiveness

--- index.js
+++ index.js
@@ -1,39 +1,16 @@
-// tabbed content
-   // http://www.entheosweb.com/tutorials/css/tabs.asp
-   $(".tab_content").hide();
-   $(".tab_content:first").show();
+var tabs = $('li');
+var tabContent = $('.tab-content');
 
- /* if in tab mode */
-   $("ul.tabs li").click(function() {
+console.log(tabContent);
 
-     $(".tab_content").hide();
-     var activeTab = $(this).attr("rel");
-     $("#"+activeTab).fadeIn();
+tabs.on('click', function() {
+  tabs.removeClass('active');
 
-     $("ul.tabs li").removeClass("active");
-     $(this).addClass("active");
+  $(this).addClass('active');
 
-   $(".tab_drawer_heading").removeClass("d_active");
-   $(".tab_drawer_heading[rel^='"+activeTab+"']").addClass("d_active");
+  tabContent.removeClass('active');
 
-   });
- /* if in drawer mode */
- $(".tab_drawer_heading").click(function() {
+  var activeTab = $(this).attr("rel");
 
-     $(".tab_content").hide();
-     var d_activeTab = $(this).attr("rel");
-     $("#"+d_activeTab).fadeIn();
-
-   $(".tab_drawer_heading").removeClass("d_active");
-     $(this).addClass("d_active");
-
-   $("ul.tabs li").removeClass("active");
-   $("ul.tabs li[rel^='"+d_activeTab+"']").addClass("active");
-   });
-
-
- /* Extra class "tab_last"
-    to add border to right side
-    of last tab */
- $('ul.tabs li').last().addClass("tab_last");
- 
+  $("#"+activeTab).addClass('active');
+});
